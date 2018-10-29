2b2e1e12984f8b3773bb82d429e99401b5148adf
janainacm <43250347+janainacm@users.noreply.github.com>
2018-10-29 17:19:33 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,6 +4,8 @@ default(fmt = :png)
 
 function main()
     # Ler dados.csv
+    x=A[:,1]
+    y=A[:,2] 
 
     kfold(x, y)
 
@@ -18,7 +20,25 @@ function main()
     plot!(xlin, ylin, c=:red, lw=2)
     png("ajuste")
 
-    # Calcule a medida R²
+    # Calcule a medida R²=1-Sr/Sq
+    m = length(y)
+    for i=1:m
+        soma+=y[i]
+    end
+    ymed=soma/m
+    for i=1:m
+         for j=1:m
+            M[i,j]=dot(y[j],yi]
+            C[i,j]=dot(y[j],y[i]
+         end
+    end
+    \betha=M\C
+    
+    for i=1:m
+           ypred +=\betha[i]*y[i]
+    end
+        
+    R2=1-norm(ypred-y)^2/norm(ymed-y)^2
 end
 
 function regressao_polinomial(x, y, p)
@@ -30,10 +50,16 @@ end
 
 function kfold(x, y; num_folds = 5, max_p=15)
     m = length(x)
-    k = div(m, num_folds)
-    I = randperm(m)
+    k = div(m, num_folds)                          # Calcule o tamanho do fold
+    I = randperm(m)                                # Embaralhe a ordem dos dados 
 
     # Seu código aqui
+   return regressao_polinomial(x, y, c)
+   ypred[i]=
+    
+   for i=1:c
+   soma=(y[i] - ypred[i])^2+soma
+   E = 1/(2*m)*soma,                               # N o número de elementos no cjto
 
     png("kfold")
 end
